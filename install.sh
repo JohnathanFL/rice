5bddfb6be1bce7476e882df525a9a643cdc8663f
Johnathan Lee <jfl747076@gmail.com>
2019-01-04 01:48:44 -0600

    Add desktop file to allow actually using the Qt-Creator-Darcula theme

--- install.sh
+++ install.sh
@@ -8,10 +8,14 @@ ln -s $RICE/zshrc ~/.zshrc
 
 # awesome configs
 rm -rf ~/.config/awesome
-ln -s `pwd`/awesome ~/.config/awesome
+ln -s $RICE/awesome ~/.config/awesome
 
 # qtcreator themes
 mkdir -p ~/.config/QtProject/qtcreator/styles
 rm ~/.config/QtProject/qtcreator/styles/darcula.xml
-ln -s `pwd`/themes/Qt-Creator-Darcula/darcula.xml ~/.config/QtProject/qtcreator/styles/darcula.xml
+ln -s $RICE/themes/Qt-Creator-Darcula/darcula.xml ~/.config/QtProject/qtcreator/styles/darcula.xml
+
+# qtcreator-themed desktop file
+rm ~/.local/share/applications/qtcreatorthemed.desktop
+ln -s $RICE/qtcreatorthemed.desktop ~/.local/share/applications/qtcreatorthemed.desktop
 
