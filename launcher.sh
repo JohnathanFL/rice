e55e0fbc3bef0b677f5206e44a4f95abf127e955
jfl747076@gmail.com <jfl747076@gmail.com>
2019-01-31 19:03:50 -0600

    Add 2-* workspaces for multimonitor (or just having 20 workspaces); Change launcher to just list commands

--- launcher.sh
+++ launcher.sh
@@ -1,14 +1,3 @@
 #!/bin/zsh
 
-DESIRED=`cat $RICE/launcher.list | fzf`
-CUTDESIRED=`echo $DESIRED | cut -d '[' -f 2 | cut -d ']' -f 1`
-
-echo DESIRED IS $DESIRED
-echo CUTDESIRED IS $CUTDESIRED
-
-if $CUTDESIRED; then
-	# Nothing for now
-	echo
-else
-	echo $CUTDESIRED | xargs -r swaymsg -t command exec
-fi
+print -rl -- ${(k)commands} | sort | fzf | xargs -r swaymsg -t command exec
