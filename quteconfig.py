4dd33fefb66d39ce06b2117feed91b408b76be3f
Johnathan Lee <jfl747076@gmail.com>
2019-05-01 22:11:12 -0500

    quteconfig.py

---
+++ quteconfig.py
@@ -0,0 +1,35 @@
+import os
+
+c.tabs.position = "left"
+c.tabs.width = '10%'
+c.tabs.show = 'switching'
+c.tabs.show_switching_delay = 1500
+#c.scrolling.smooth = True
+c.qt.low_end_device_mode = "never"
+
+c.completion.shrink = True
+c.content.pdfjs = True
+
+c.qt.args += ' /usr/lib/qt/plugins/ppapi/libwidevinecdm.so'
+
+
+bindings = {
+    "<Ctrl+Tab>": "tab-next",
+    "<Ctrl+Shift+Tab>": "tab-prev",
+}
+
+delBindings = [
+        "j", "k", "h", "l", "J", "K"
+
+]
+
+
+for binding in delBindings:
+    config.unbind(binding)
+
+for key, bind in bindings.items():
+    config.bind(key, bind)
+
+
+# THEME
+config.source(os.environ['RICE'] + '/nord-qutebrowser.py')
