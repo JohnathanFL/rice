2e2c0d40b7e2299bffc39a23801a0ca8c0f381a2
Johnathan Lee <jfl747076@gmail.com>
2019-05-06 08:40:23 -0500

    Clean up screenshot.sh style

--- screenshot.sh
+++ screenshot.sh
@@ -5,7 +5,9 @@
 FILENAME=`date +'printscr_%F_%s.png'`
 mkdir -p ~/Pictures/Screenshots
 
-[ ! -z $(which slurp) ] && \
-	grim -g "$(slurp)" ~/Pictures/Screenshots/$FILENAME \
-||      grim ~/Pictures/Screenshots/$FILENAME
+if [ ! -z $(which slurp) ]; then
+	grim -g "$(slurp)" ~/Pictures/Screenshots/$FILENAME
+else
+	grim ~/Pictures/Screenshots/$FILENAME
+fi
 
