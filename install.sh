a3b2ba94fea5e9ae6930ee4a1d5f70d75ada0621
JohnathanFL <jfl747076@gmail.com>
2019-06-11 22:01:28 -0500

    Add mpd.conf

--- install.sh
+++ install.sh
@@ -2,48 +2,48 @@
 
 RICE=`pwd`
 
-# zshrc
+echo zshrc
 rm ~/.zshrc
 ln -s $RICE/zshrc ~/.zshrc
 
-# awesome configs
+echo awesome configs
 rm -rf ~/.config/awesome
 ln -s $RICE/awesome ~/.config/awesome
 
-# sway configs
+echo sway configs
 rm -rf ~/.config/sway
 ln -s $RICE/sway ~/.config/sway
 
-# waybar
+echo waybar
 rm -rf ~/.config/waybar
 ln -s $RICE/waybar ~/.config/waybar
 
-# launcher
+echo launcher
 rm ~/bin/launcher.sh
 ln -s $RICE/launcher.sh ~/bin/launcher.sh
 
-# qtcreator themes
+echo qtcreator themes
 mkdir -p ~/.config/QtProject/qtcreator/styles
 rm ~/.config/QtProject/qtcreator/styles/darcula.xml
 ln -s $RICE/themes/Qt-Creator-Darcula/darcula.xml ~/.config/QtProject/qtcreator/styles/darcula.xml
 
-# qtcreator-themed desktop file
+echo qtcreator-themed desktop file
 rm ~/.local/share/applications/qtcreatorthemed.desktop
 ln -s $RICE/qtcreatorthemed.desktop ~/.local/share/applications/qtcreatorthemed.desktop
 
-# qtcreator-themed script
+echo qtcreator-themed script
 rm ~/bin/qtcreatorthemed
 ln -s $RICE/qtcreatorthemed ~/bin/qtcreatorthemed
 
-# and termite, for good measure
+echo and termite, for good measure
 rm -rf ~/.config/termite
 ln -s $RICE/termite ~/.config/termite
 
-# wayfire
+echo wayfire
 rm ~/.config/wayfire.ini
 ln -s $RICE/wayfire.ini ~/.config/
 
-# wallpaper setter
+echo wallpaper setter
 rm ~/bin/setwall
 ln -s $RICE/setwall ~/bin/
 
@@ -51,7 +51,11 @@ echo "Installing /etc/issue"
 sudo rm /etc/issue
 sudo ln -s $RICE/issue /etc/issue
 
-# qutebrowser
+echo qutebrowser
 mkdir -p ~/.config/qutebrowser
 rm ~/.config/qutebrowser/config.py
 ln -s $RICE/quteconfig.py ~/.config/qutebrowser/config.py
+
+echo mpd.conf
+mkdir -p ~/.config/mpd
+ln -s $RICE/mpd.conf ~/.config/mpd/mpd.conf
