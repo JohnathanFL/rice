a2b05b9e55c95496380180cb7929c91d2b94aed3
JohnathanFL <jfl747076@gmail.com>
2019-09-30 08:27:04 -0500

    Move the commands to launcher.sh because I'm dumb

--- launcher.sh
+++ launcher.sh
@@ -1,2 +1,6 @@
 #!/bin/sh
-setsid volund
+#setsid volund
+
+pkill j4-dmenu-desktop
+rm /tmp/j4-waiter
+j4-dmenu-desktop --wait-on=/tmp/j4-waiter --dmenu="bemenu -b --list 10 --ignorecase --nb \#3b4252 --hb \#5e81ac --hf \#d8dee9 --fb \#434c5e --ff \#d8dee9 -p ''"
