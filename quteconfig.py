56b9dbb9b4b10fff30b0abd0afa5a09a2e58a3d0
JohnathanFL <jfl747076@gmail.com>
2019-10-27 00:13:26 -0500

    Make qutebrowser use yet another color for non-container tabs

--- quteconfig.py
+++ quteconfig.py
@@ -17,12 +17,15 @@ c.qt.process_model = 'process-per-site-instance'
 bindings = {
     "<Ctrl+Tab>": "tab-next",
     "<Ctrl+Shift+Tab>": "tab-prev",
-    "<F1>": "spawn mpv {url} --gpu-api=vulkan --gpu-context=waylandvk --ytdl --ytdl-raw-options=yes-playlist= --force-window"
+    "<F1>": "spawn mpv {url} --gpu-api=vulkan --gpu-context=waylandvk --ytdl --ytdl-raw-options=yes-playlist= --force-window",
+    "j": "scroll-px 0 50",
+    "k": "scroll-px 0 -50",
+    "J": "scroll-px 0 800",
+    "K": "scroll-px 0 -800",
 }
 
 delBindings = [
         "j", "k", "h", "l", "J", "K"
-
 ]
 
 
@@ -37,6 +40,9 @@ for key, bind in bindings.items():
 config.source(os.environ['RICE'] + '/themes/nord-qutebrowser.py')
 
 # Use a different color for work container to give visual distinction
-if os.environ['QUTE_CONTAINER'] == "work":
+if "QUTE_CONTAINER" not in os.environ:
+    c.colors.tabs.selected.even.bg = "#B48EAD"
+elif os.environ['QUTE_CONTAINER'] == "work":
     c.colors.tabs.selected.even.bg = "#d08770" # nord12
-    c.colors.tabs.selected.odd.bg = c.colors.tabs.selected.even.bg
+
+c.colors.tabs.selected.odd.bg = c.colors.tabs.selected.even.bg
