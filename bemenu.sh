2fc0b2346c7ca62b225bc6f16d933d5b15faa0ee
JohnathanFL <jfl747076@gmail.com>
2020-02-16 15:37:47 -0600

    WIP: Monokai bemenu

--- bemenu.sh
+++ bemenu.sh
@@ -1,17 +1,36 @@
-#!/bin/sh
+#!/bin/zsh
 # bemenu wrapper with my personalized settings
 
+# MONOKAI THEME
+font="Victor mono 10"
+titleBg=$MONOKAI_RED
+titleFg=$MONOKAI_WHITE
+filterBg=$MONOKAI_BLACK
+filterFg=$MONOKAI_WHITE
+normalBg=$MONOKAI_BLACK
+normalFg=$MONOKAI_WHITE
+highlightBg=$MONOKAI_GREY
+highlightFg=$MONOKAI_WHITE
+selectedBg=$MONOKAI_BLUE
+selectedFg=$MONOKAI_BLUE
+scrollbarBg=$MONOKAI_PURPLE
+scrollbarFg=$MONOKAI_WHITE
 
 bemenu -b \
   --list 10 \
   --ignorecase \
-  --nb \#3b4252 \
-  --hb \#5e81ac \
-  --hf \#d8dee9 \
-  --fb \#434c5e \
-  --ff \#d8dee9 \
-  --tb \#3b4252 \
-  --tf \#d8dee9 \
+  --fn $font \
+  --tb $titleBg \
+  --tf $titleFg \
+  --fb $filterBg \
+  --ff $filterFg \
+  --nb $normalBg \
+  --nf $normalFg \
+  --hb $highlightBg \
+  --hf $highlightFg \
+  --sb $selectedBg \
+  --sf $selectedFg \
+  --scb $scrollbarBg \
+  --scf $scrollbarFg \
   -p '' \
-  --fn 'VictorMono 10' \
   $@
