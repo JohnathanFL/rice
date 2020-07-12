be1a7540e6d52c99856e9d6012dd6949478776cd
JohnathanFL <jfl747076@gmail.com>
2020-07-12 02:09:15 -0500

    Remove references to ~/.configs, replacing with ~/rice

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -1,46 +1,7 @@
-set -x RICE ~/.configs
-set -x fish_user_paths ~/bin /bin /usr/bin /usr/local/bin ~/go/bin/ ~/.nimble/bin/ ~/.cargo/bin
-set -x TERMINAL kitty
-set -x PKGEXT ".pkg.tar"
-
-set -x EDITOR kak
 alias e $EDITOR
 alias sway 'set_wayland on; /usr/bin/sway'
 alias hikari 'set_wayland on; /usr/bin/hikari'
 alias startx 'set_wayland off; /bin/startx'
 
 set -x CGO_ENABLED 1
-
-
-set -x NORD_DARK0 '#2e3440'
-set -x NORD_DARK1 '#3b4252'
-set -x NORD_DARK2 '#434c5e'
-set -x NORD_DARK3 '#4c566a'
-
-# Lights
-set -x NORD_LIGHT0 '#d8dee9'
-set -x NORD_LIGHT1 '#e5e9f0'
-set -x NORD_LIGHT2 '#eceff4'
-
-# Frosts
-set -x NORD_FROST0 '#8fbcbb'
-set -x NORD_FROST1 '#88c0d0'
-set -x NORD_FROST2 '#81a1c1'
-set -x NORD_FROST3 '#5e81ac'
-
-set -x NORD_AURORA0 '#bf616a'
-set -x NORD_AURORA1 '#d08770'
-set -x NORD_AURORA2 '#ebcb8b'
-set -x NORD_AURORA3 '#a3be8c'
-set -x NORD_AURORA4 '#b48ead'
-
-set -x MONOKAI_BLACK '#272822'
-set -x MONOKAI_WHITE '#f8f8f2'
-set -x MONOKAI_GREY '#74715e'
-set -x MONOKAI_RED '#F92672'
-set -x MONOKAI_BLUE '#66D9EF'
-set -x MONOKAI_GREEN '#A6E22E'
-set -x MONOKAI_ORANGE '#FD971F'
-set -x MONOKAI_PURPLE '#ae81ff'
-set -x MONOKAI_YELLOW '#e6db74'
 
