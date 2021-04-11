2abc51a4ffc7fb068f0f6d314d451e4491d08f3a
Johnathan Lee <jfl747076@gmail.com>
2021-04-10 20:56:23 -0500

    Allow insta-screenshot

--- bin/screenshot.sh
+++ bin/screenshot.sh
@@ -3,11 +3,13 @@
 # TODO: Make it autodetect X11 or Wayland
 
 FILENAME=`date +'printscr_%F_%s.png'`
+FILE="$HOME/Pictures/Screenshots/$FILENAME"
 mkdir -p ~/Pictures/Screenshots
 
-if [ ! -z $(which slurp) ]; then
-	grim -g "$(slurp)" ~/Pictures/Screenshots/$FILENAME
+if [ "$1" != "now" ] && [ ! -z $(which slurp) ]; then
+	grim -g "$(slurp)" "$FILE"
 else
-	grim ~/Pictures/Screenshots/$FILENAME
+	grim "$FILE"
 fi
 
+cat "$FILE" | wl-copy --type image/png
