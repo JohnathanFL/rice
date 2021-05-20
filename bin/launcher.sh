1d167ac87cd13b05b7468ab793843b0122f98575
Johnathan Lee <jfl747076@gmail.com>
2021-05-19 22:31:20 -0500

    Fix launcher on fish

--- bin/launcher.sh
+++ bin/launcher.sh
@@ -1,9 +1,13 @@
 #!/bin/sh
 #setsid volund
 
+# Really dumb bug: j4 relies on executing the shell for *everything*. Thus,
+# a non-compliant shell like fish will break it.
+export SHELL=/bin/sh
 
 exec j4-dmenu-desktop \
   --no-generic \
   --term=kitty \
   --usage-log=$HOME/.local/share/launcher-usage \
-  --dmenu="bemenu.sh"
+  --dmenu="bemenu.sh"\
+  $@
