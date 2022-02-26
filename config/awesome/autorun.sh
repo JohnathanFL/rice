15cd6395b45a722f23b7699d5594e3dc8a2645ca
Johnathan Lee <JFL747076@gmail.com>
2022-02-26 12:18:44 -0600

    Tweak mouse in awesomewm

--- config/awesome/autorun.sh
+++ config/awesome/autorun.sh
@@ -11,3 +11,5 @@ run cbatticon
 run feh --bg-scale $RICE/wall --B \#2e3440
 
 xrandr --output DVI-D-1 --primary --pos 0x0 --output HDMI-2  --right-of DVI-D-1
+xinput set-prop "Logitech Gaming Mouse G502" "Coordinate Transformation Matrix" 2.4 0 0 0 2.4 0 0 0 1
+xset r rate 150 50
