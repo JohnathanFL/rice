fb1bd34db018d5a56824f92e217a87946f0a88e9
Johnathan Lee <jfl747076@gmail.com>
2023-11-24 22:44:22 -0600

    Group screenshots by Y/M/D as dirs

--- bin/screenshot.sh
+++ bin/screenshot.sh
@@ -1,12 +1,13 @@
 #!/bin/sh
+set -eu
 
 # TODO: Make it autodetect X11 or Wayland
 
-FILENAME=`date +'printscr_%F_%s.png'`
+FILENAME=`date +'%Y/%m/%d/printscr_%s.png'`
 FILE="$HOME/Pictures/Screenshots/$FILENAME"
-mkdir -p ~/Pictures/Screenshots
+mkdir -p "$(dirname "$FILE")"
 
-if [ "$1" != "now" ] && [ ! -z $(which slurp) ]; then
+if [ "${1:-}" != "now" ] && [ ! -z $(which slurp) ]; then
 	grim -g "$(slurp)" "$FILE"
 else
 	grim "$FILE"
